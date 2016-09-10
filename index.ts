14ce2d928ec1849c792b20844275861769b3a3d6
Dominik Palo <dominik@palo.sk>
2016-09-10 20:36:23 +0200

    Use socket.io for web <-> server communication instead of AJAX

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import express = require("express");
 import http = require("http");
 import socketIo = require("socket.io");
 import util = require("util");
-import bodyParser = require("body-parser");
 
 import Socket = SocketIO.Socket;
 
@@ -13,31 +12,22 @@ const io = socketIo(server);
 
 let clientSocket: Socket;
 
-
 app.use(express.static(__dirname + '/static'));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/', (req, res) => {
-    res.sendStatus(200);
-
-    if (clientSocket) {
-        clientSocket.emit("setPin", req.body);
-    }
-});
-
 io.on('connection', (socket: Socket) => {
     console.log('New connection');
     clientSocket = socket;
+
+    clientSocket.on("webMessage", (payload) => {
+        console.log("Message received from web: ", payload)
+    });
 });
+
+
 
 server.listen(3000, () => {
     console.log('Example app listening on port 3000!');
